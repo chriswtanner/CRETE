7b0902d84c45ba4ce5b627e725691ed3be167581
chriswtanner <christanner@cs.brown.edu>
2018-08-12 13:56:57 -0400

    asdf

--- src/CCNN.py
+++ src/CCNN.py
@@ -543,11 +543,17 @@ class CCNN:
 		goldenSuperSet = {}
 
 		# sanity check: ensures our DataHandler's XUID's matches the WD ones we import
-		#for xuid in self.dh.devXUIDs:
-		#	if 
-
+		for xuid in xuidsFromPredictions:
+			if xuid not in self.dh.devXUIDs:
+				print("* ERROR: xuid (from predictions) isn't in dh.devXUIDs")
+			 	exit(1)
+				m = self.corpus.XUIDToMention[xuid]
+				if m.dir_num not in self.helper.devDirs:
+					print("* ERROR: xuid's mention is from a dir other than helper.devDirs")
+					exit(1)
 		exit(1)
 
+		'''
 		XUIDToDocs = defaultdict(set)
 		for dir_num in dirToXUIDPredictions.keys():
 			print("-----------\ncurrent dir_num:", str(dir_num), "\n-----------")
