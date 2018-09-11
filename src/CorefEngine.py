ff95a803a81cdb412af343d677891e0d86a2b8ba
chriswtanner <christanner@cs.brown.edu>
2018-09-10 21:30:33 -0400

    sdf

--- src/CorefEngine.py
+++ src/CorefEngine.py
@@ -79,7 +79,7 @@ class CorefEngine:
 		args = params.setCorefEngineParams()
 		kbp_parser = KBPParser(args, "../data/KBP/")
 		kbp_parser.parseCorpus()
-		exit(1)
+		exit(1) d
 		'''
 		start_time = time.time()
 		# parses elmo embeddings
